40ce5b5ddc143fe8867020fd8868fc9090246a9b
Vinicius Pacheco <vpacheco0427@gmail.com>
2021-11-18 23:54:49 -0300

    add readme

--- Fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-2-javascript-trabalhando-com-elementos/script.js
+++
@@ -1,7 +0,0 @@
-
-      let numArray = ['um', 'dois', 'três', 'quatro', 'cinco', 'seis', 'sete', 'oito', 'nove', 'dez'];
-      for (let i = 0; i < numArray; i += 1) {
-        let temp = numArray[i]
-        
-      }
-// lista.appendChild(addList(temp));
