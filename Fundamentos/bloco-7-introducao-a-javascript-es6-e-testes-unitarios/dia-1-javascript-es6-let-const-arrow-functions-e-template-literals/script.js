bf7cb9518180c66eb58e4944bb39a9641a4578d2
Vinicius Pacheco <vpacheco0427@gmail.com>
2021-12-03 16:29:18 -0300

    part 1

---
+++ Fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/script.js
@@ -0,0 +1,19 @@
+const testingScope = (escopo) => {
+    if (escopo === true) {
+      let ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
+    //   ifScope = ifScope + ' ótimo, fui utilizada no escopo !';
+      console.log(`${ifScope} ótimo, fui utilizada no escopo`);
+    } else {
+      let elseScope = 'Não devo ser utilizada fora meu escopo (else)';
+      console.log(elseScope);
+    }
+    //  console.log(ifScope + ' o que estou fazendo aqui ? :O'); // Se necessário esta linha pode ser removida.
+  }
+
+  testingScope(true);
+
+  const oddsAndEvens = [13, 3, 4, 10, 7, 2];
+
+  oddsAndEvens.sort(function(a, b){return a-b});
+
+  console.log(`Os números ${oddsAndEvens} se encontram ordenados de forma crescente!`); // será necessário alterar essa linha 😉
