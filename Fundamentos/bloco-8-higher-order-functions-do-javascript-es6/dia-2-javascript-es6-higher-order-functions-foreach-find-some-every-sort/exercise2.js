a9bd1b888baf849aca9310aa1f00ca923a75f63d
Vinicius Pacheco <vpacheco0427@gmail.com>
2021-12-12 20:57:41 -0300

    try exercise 2 8.2

--- Fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-2-javascript-es6-higher-order-functions-foreach-find-some-every-sort/exercise2.js
+++ Fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-2-javascript-es6-higher-order-functions-foreach-find-some-every-sort/exercise2.js
@@ -66,10 +66,15 @@ const books = [
 //   2 - Retorne o nome do livro de menor nome.
 // Dica: use a função forEach .
 
-function smallerName() {
-  let nameBook;
-  // escreva aqui o seu código
-
+function smallerName(item) {
+  let nameBook = item.name;
+  for (let i = 0; i < books.length; i += 1) {
+    if (item[i].name.length < nameBook.length) {
+     nameBook = item[i].name;
+    }
+  }
   // Variável nameBook que receberá o valor do menor nome;
   return nameBook;
 }
+
+console.log(books.forEach(smallerName));
