07d93d961e45cd32faa1bde87dc15573a3fe7225
Vinicius Pacheco <vpacheco0427@gmail.com>
2022-01-27 23:23:09 -0300

    criando component

---
+++ Front_end/bloco-10-introducao-a-react/dia-1-hello-world-no-react/vini-app/src/Component.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+class Component extends React.Component {
+    render() {
+      return <div>
+        <h1>Vinicius Pacheco Franco</h1>
+        <h2>Software Developer Studant</h2>
+      </div> 
+    }
+}
+
+export default Component
