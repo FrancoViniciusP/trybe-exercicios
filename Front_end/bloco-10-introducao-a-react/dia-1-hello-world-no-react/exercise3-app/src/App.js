43bced676d65882382df8ff98896c29d89a6cca6
Vinicius Pacheco <vpacheco0427@gmail.com>
2022-01-28 14:30:54 -0300

    exercise 4 10.1

--- Front_end/bloco-10-introducao-a-react/dia-1-hello-world-no-react/exercise3-app/src/App.js
+++ Front_end/bloco-10-introducao-a-react/dia-1-hello-world-no-react/exercise3-app/src/App.js
@@ -5,11 +5,11 @@ import Content from './Content';
 import Footer from './Footer';
 
 function App() {
-  return (<div>
-      <Header/>
-      <Content/>
-      <Footer/>
-      </div>)
+  return <div>
+           <Header/>
+           <Content/>
+           <Footer/>
+         </div>
 }
 
 export default App;
--- Front_end/bloco-10-introducao-a-react/dia-1-hello-world-no-react/exercise3-app/src/Footer.jsx
+++ Front_end/bloco-10-introducao-a-react/dia-1-hello-world-no-react/exercise3-app/src/Footer.jsx
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 
 class Footer extends Component {
     render() {
-        return <h1>E isso é só o começo...</h1>
+        return <footer className='footer'>E isso é só o começo...</footer>
     }
 }
 
