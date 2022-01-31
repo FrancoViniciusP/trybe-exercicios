b470e9ec1512122a48cbb99e87791679f648fe34
Vinicius Pacheco <vpacheco0427@gmail.com>
2022-01-31 17:00:17 -0300

    exercicios de fixaçao

--- Front_end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-1-componentes-com-estado-e-eventos/fancy-buttons/src/App.js
+++ Front_end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-1-componentes-com-estado-e-eventos/fancy-buttons/src/App.js
@@ -20,31 +20,45 @@ class App extends Component {
     this.handleClick1 = this.handleClick1.bind(this);
     this.handleClick2 = this.handleClick2.bind(this);
     this.handleClick3 = this.handleClick3.bind(this);
-    console.log(this);
+    this.state = {
+      numeroDeCliques1: 0,
+      numeroDeCliques2: 0,
+      numeroDeCliques3: 0,
+    }
   }
 
   handleClick1() {
-    console.log(this);
     console.log("clicou no botão 1");
+    this.setState(({ numeroDeCliques1 }, _props) => 
+      ({numeroDeCliques1: numeroDeCliques1 + 1})
+    );
+    console.log(this.state.numeroDeCliques1); 
   }
+ 
+  // o valor é impresso no console antes da atualização do state, devido a assincronissidade da função setState.
   
   handleClick2() {
-    console.log(this);
     console.log("clicou no botão 2");
+    this.setState(({ numeroDeCliques2 }, _props) => 
+      ({numeroDeCliques2: numeroDeCliques2 + 1})
+    );
+    console.log(this.state.numeroDeCliques2);
   }
-
+  
   handleClick3() {
-    console.log(this);
     console.log("clicou no botão 3");
+    this.setState(({ numeroDeCliques3 }, _props) => 
+      ({numeroDeCliques3: numeroDeCliques3 + 1})
+    );
+    console.log(this.state.numeroDeCliques3);
   }
   
   render() {
-    console.log(this);
     return (
       <div className="App">
-        <button onClick={this.handleClick1} >Botão 1</button>
-        <button  onClick={this.handleClick2} >Botão 2</button>
-        <button  onClick={this.handleClick3} >Botão 3</button>
+        <button className='red' onClick={this.handleClick1} >{this.state.numeroDeCliques1}</button>
+        <button className='blue' onClick={this.handleClick2} >{this.state.numeroDeCliques2}</button>
+        <button className='green' onClick={this.handleClick3} >{this.state.numeroDeCliques3}</button>
       </div>
     );
   }
