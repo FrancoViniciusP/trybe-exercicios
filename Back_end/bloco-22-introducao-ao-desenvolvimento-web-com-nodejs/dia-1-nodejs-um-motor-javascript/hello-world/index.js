113b0b63e512abfcc6e0f2aeddc6b57a5c02a51d
Vinicius Pacheco <vpacheco0427@gmail.com>
2022-05-09 15:42:30 -0300

    feat: change the hello message

--- Back_end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-1-nodejs-um-motor-javascript/hello-world/index.js
+++ Back_end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-1-nodejs-um-motor-javascript/hello-world/index.js
@@ -1,1 +1,8 @@
-console.log('Hello, world!');
+// console.log('Hello, world!');
+
+const readline = require('readline-sync');
+
+const name = readline.question('What is your name? ');
+const age = readline.questionInt('How old are you? ');
+
+console.log(`Hello, ${name}! You are ${age} years old!`);
