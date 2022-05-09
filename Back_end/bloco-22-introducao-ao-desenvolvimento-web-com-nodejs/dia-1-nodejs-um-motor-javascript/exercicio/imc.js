ea6a494932a8d056c19394c7d340e8e464190421
Vinicius Pacheco <vpacheco0427@gmail.com>
2022-05-09 19:15:41 -0300

    feat: use the terminal to ask the user the values

--- Back_end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-1-nodejs-um-motor-javascript/exercicio/imc.js
+++ Back_end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-1-nodejs-um-motor-javascript/exercicio/imc.js
@@ -1,5 +1,10 @@
+const readline = require('readline-sync');
+
+const weigth = readline.questionFloat('Qual o seu peso em kg?');
+const height = readline.questionFloat('Qual sua altura em metros?');  
+
 const imcCalc = (weight, height) => {
     return weight / height**2
 }
 
-console.log(imcCalc(65, 1.85));
+console.log(imcCalc(weigth, height));
