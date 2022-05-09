aef9b1fed430c5ea7bec25dbbe96dda9299305bd
Vinicius Pacheco <vpacheco0427@gmail.com>
2022-05-09 19:30:09 -0300

    feat: print the imc and the category

--- Back_end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-1-nodejs-um-motor-javascript/exercicio/imc.js
+++ Back_end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-1-nodejs-um-motor-javascript/exercicio/imc.js
@@ -4,7 +4,17 @@ const weigth = readline.questionFloat('Qual o seu peso em kg?');
 const height = readline.questionFloat('Qual sua altura em metros?');  
 
 const imcCalc = (weight, height) => {
-    return weight / height**2
+    const imc = weight / height**2;
+    return `IMC = ${imc} e sua situação é ${imcTable(imc)}`;
+}
+
+const imcTable = (imcValue) => {
+    if (imcValue < 18.5) return 'Abaixo do peso (magreza)';
+    if (imcValue > 18.5 && imcValue < 24.9) return 'Peso normal';
+    if (imcValue > 24.9 && imcValue < 29.9) return 'Acima do peso (sobrepeso)';
+    if (imcValue > 29.9 && imcValue < 34.9) return 'Obesidade grau I';
+    if (imcValue > 34.9 && imcValue < 39.9) return 'Obesidade grau II';
+    else return 'Obesidade graus III e IV';    
 }
 
 console.log(imcCalc(weigth, height));
