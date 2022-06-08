187a3fe260f4353a4a4d758795f48bd5c9ce182f
Vinicius Pacheco <vpacheco0427@gmail.com>
2022-06-08 10:09:27 -0300

    dev: create a new column

--- Back_end/bloco-24-nodejs-orm-autenticacao/dia-1-orm-interface-da-aplicacao-com-o-banco-de-dados/app-with-sequelize/models/user.js
+++ Back_end/bloco-24-nodejs-orm-autenticacao/dia-1-orm-interface-da-aplicacao-com-o-banco-de-dados/app-with-sequelize/models/user.js
@@ -2,8 +2,9 @@ const User = (sequelize, DataTypes) => {
   const User = sequelize.define("User", {
     fullName: DataTypes.STRING,
     email: DataTypes.STRING,
+    phone_num: DataTypes.STRING,
   });
-  
+
   return User;
 };
 
